49acb6cee279ceba12b15c9bb8dc67c01a180947
Max32pl <107699980+Max32pl@users.noreply.github.com>
2022-06-20 10:53:39 +0200

    Add files via upload

---
+++ 2_AdaBoost(vc,f,ap,T)_Model.py
@@ -0,0 +1,117 @@
+import pandas as pd
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+import math
+import numpy as np
+from geneticalgorithm import geneticalgorithm as ga
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LinearRegression
+import pickle
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+import os
+
+def dataEinlesen():
+
+    data_df = pd.read_csv(r"C:\Users\max_x\OneDrive\Desktop\Uni\SA\Programmierung\EigeneMethode - MP\Projekt-SA"
+                          r"\Versuche_Mod_Mar.csv",sep=";")
+    #print(data_df)
+
+    data_X = data_df[["f [mm]","T [C]","ap [mm]","vc [m/min]"]]
+    data_y = data_df[["Ferritsc [%]"]]
+    print("\nEingang:",data_X)
+    print("\nAusgang:",data_y)
+
+    return data_X,data_y
+
+def adaBoostRegressor(data_X, data_y):
+
+    #Speicherort von Modell und Daten
+    path = r"C:\Users\max_x\OneDrive\Desktop\Uni\SA\Programmierung\EigeneMethode - MP"
+    #Liste zum Abspeichern des trainierten Modells der einzelnen Durchgänge der Kreuzvalidierung
+    filename = ["2_Model_AdaBoost_vc_ap_f_T_Nr1","2_Model_AdaBoost_vc_ap_f_T_Nr2","2_Model_AdaBoost_vc_ap_f_T_Nr3"
+        ,"2_Model_AdaBoost_vc_ap_f_T_Nr4","2_Model_AdaBoost_vc_ap_f_T_Nr5"]
+    #Liste zum Abspeichern der der Testdaten
+    testdata_x_list = ["2_testdata_X_AdaBoost_vc_ap_f_T_Nr1","2_testdata_X_AdaBoost_vc_ap_f_T_Nr2","2_testdata_X_AdaBoost_vc_ap_f_T_Nr3"
+        ,"2_testdata_X_AdaBoost_vc_ap_f_T_Nr4","2_testdata_X_AdaBoost_vc_ap_f_T_Nr5"]
+    testdata_y_list = ["2_testdata_y_AdaBoost_vc_ap_f_T_Nr1", "2_testdata_y_AdaBoost_vc_ap_f_T_Nr2","2_testdata_y_AdaBoost_vc_ap_f_T_Nr3"
+        ,"2_testdata_y_AdaBoost_vc_ap_f_T_Nr4","2_testdata_y_AdaBoost_vc_ap_f_T_Nr5"]
+    # Liste zum Abspeichern der der Trainingsdaten
+    traindata_x_list = ["2_traindata_X_AdaBoost_vc_ap_f_T_Nr1","2_traindata_X_AdaBoost_vc_ap_f_T_Nr2","2_traindata_X_AdaBoost_vc_ap_f_T_Nr3"
+        ,"2_traindata_X_AdaBoost_vc_ap_f_T_Nr4","2_traindata_X_AdaBoost_vc_ap_f_T_Nr5"]
+    traindata_y_list = ["2_traindata_y_AdaBoost_vc_ap_f_T_Nr1","2_traindata_y_AdaBoost_vc_ap_f_T_Nr2","2_traindata_y_AdaBoost_vc_ap_f_T_Nr3"
+        ,"2_traindata_y_AdaBoost_vc_ap_f_T_Nr4","2_traindata_y_AdaBoost_vc_ap_f_T_Nr5"]
+    #Variable zum Durchgänge zählen
+    i = 0
+
+    rn = range(1, 72)
+
+    kf5 = KFold(n_splits=5, shuffle=True)
+
+    for train_index, test_index in kf5.split(rn):
+
+        print("\nDurchlauf ", i+1, ":")
+
+        # print("Train:",train_index,"\nTest:",test_index)
+        X_train = data_X.iloc[train_index]
+        X_train = X_train.to_numpy()
+        X_test = data_X.iloc[test_index]
+        X_test = X_test.to_numpy()
+        y_train = data_y.iloc[train_index]
+        y_train = y_train.to_numpy()
+        y_train = y_train.flatten()
+        y_test = data_y.iloc[test_index]
+        y_test = y_test.to_numpy()
+        y_test = y_test.flatten()
+
+        # print("Test:",y_test)
+        # print("Train X:",len(X_train),"Train y:",len(y_train))
+
+        #Trainieren des Modells
+        regrM = AdaBoostRegressor(DecisionTreeRegressor(max_depth=5), random_state=None, n_estimators=20)
+        regrM.fit(X_train, y_train)
+
+        y_pred_test = regrM.predict(X_test)
+        y_pred_train = regrM.predict(X_train)
+
+        #Güte für Testdaten
+        abserror = mean_absolute_error(y_test, y_pred_test)
+        print("Abs Error Test: ", abserror)
+        rmse = mean_squared_error(y_test, y_pred_test, squared=False)
+        print("RMSE Test:", rmse)
+        r2_Score = r2_score(y_test, y_pred_test)
+        print("r2_score Test:", r2_Score)
+
+        #Güte für Trainingsdaten
+        abserror = mean_absolute_error(y_train, y_pred_train)
+        print("Abs Error Train: ", abserror)
+        rmse = mean_squared_error(y_train, y_pred_train, squared=False)
+        print("RMSE Train:", rmse)
+        r2_Score = r2_score(y_train, y_pred_train)
+        print("r2_score Train:", r2_Score)
+
+        #Modell speichern in .sav Datei
+        file = os.path.join(path, filename[i])
+        pickle.dump(regrM, open(file, 'wb'))
+        
+        #Datensatz speichern in .sav Datei
+        file = os.path.join(path, testdata_y_list[i])
+        pickle.dump(y_test, open(file, 'wb'))
+        file = os.path.join(path, testdata_x_list[i])
+        pickle.dump(X_test, open(file, 'wb'))
+        file = os.path.join(path, traindata_y_list[i])
+        pickle.dump(y_train, open(file, 'wb'))
+        file = os.path.join(path, traindata_x_list[i])
+        pickle.dump(X_train, open(file, 'wb'))
+
+        i += 1
+
+if __name__ == '__main__':
+
+    data_X,data_y = dataEinlesen()
+
+    adaBoostRegressor(data_X,data_y)
---
+++ 2_Optimierung(vc,f,ap,T)_ga.py
@@ -0,0 +1,130 @@
+import pandas as pd
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+import math
+import numpy as np
+from geneticalgorithm import geneticalgorithm as ga
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LinearRegression
+import pickle
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+import os
+
+def f(X):
+
+    #ga variiert ganze Zahlen, 2. Stelle nach Komma wird benötigt
+    X[0] = X[0]/100
+    X[2] = X[2]/100
+
+    #Eingangsvariable für Prediction von Rz
+    input_pred_rz = X[0]
+    input_pred_rz = np.reshape(input_pred_rz,(-1,1))
+
+    #Eingangsvariable für Prediction von Martensit
+    #3. und 4. Stelle zum Einstellen von ap und vc nutzen
+    input_pred_m = [[X[0], X[1], X[2], X[3]]]
+
+    #Laden des gespeicherten Modells AdaBoost/Martensit
+    filename = ["2_Model_AdaBoost_vc_ap_f_T_Nr1", "2_Model_AdaBoost_vc_ap_f_T_Nr2", "2_Model_AdaBoost_vc_ap_f_T_Nr3"
+        , "2_Model_AdaBoost_vc_ap_f_T_Nr4", "2_Model_AdaBoost_vc_ap_f_T_Nr5"]
+    file = os.path.join(path, filename[3])
+    loaded_model = pickle.load(open(file, 'rb'))
+
+    #Prediction Martensit
+    m_pred = loaded_model.predict(input_pred_m)
+
+    #Laden des gespeicherten Modells LinReg/Rauheit
+    loaded_model = pickle.load(open('model_lin_Rz', 'rb'))
+
+    #Prediction Rauheit
+    r_pred = loaded_model.predict(input_pred_rz)
+
+    """Gewichtung theoretische Rauheit & Martensitbildung"""
+    y = (w1*abs(m_target-m_pred))+(w2*abs(r_target-r_pred))
+
+    return y
+
+if __name__ == '__main__':
+
+    #Optimierung
+
+    """Speicherort von Modell und Daten"""
+    global path
+    path = r"C:\Users\max_x\OneDrive\Desktop\Uni\SA\Programmierung\EigeneMethode - MP"
+
+    """Zielgrößen, Benutzereingaben"""
+    global w1
+    w1 = 0.5
+    global w2
+    w2 = 0.5
+    global m_target
+    m_target = 4
+    global r_target
+    r_target = 20
+
+    X = []
+    varbound = np.array([[10, 70], [-150, -50],[20, 50],[20, 70]])
+    vartype = np.array([['int'], ['int'], ['int'], ['int']])
+
+    algorithm_param = {'max_num_iteration': None,
+                       'population_size': 100,
+                       'mutation_probability': 0.5,
+                       'elit_ratio': 0.01,
+                       'crossover_probability': 0.5,
+                       'parents_portion': 0.3,
+                       'crossover_type': 'uniform',
+                       'max_iteration_without_improv': 200}
+
+    model = ga(function=f,
+               dimension=4,
+               variable_type_mixed=vartype,
+               variable_boundaries=varbound,
+               algorithm_parameters=algorithm_param)
+
+
+    model.run()
+
+    #Formatieren des Lösungsoutputs in numpy
+    solution_list = model.output_dict['variable']
+    #solution_list = np.append(solution_list,[0.3,50])
+    #ga variiert ganze Zahlen, 2. Stelle nach Komma wird benötigt
+    solution_list[0] = solution_list[0]/100
+    solution_list[2] = solution_list[2] / 100
+    print('\nLösung: ', solution_list)
+
+    #Ausgabe der pred. Rauheit & Differenz zum Soll
+    loaded_model = pickle.load(open('model_lin_Rz', 'rb'))
+    r_pred = loaded_model.predict(solution_list[0].reshape(1,-1))
+    print("\nRauheit pred: ",r_pred)
+    print('Rauheit soll: ', r_target)
+
+    r_diff = abs(r_target-r_pred)
+    print("Rauheit Diff: ",r_diff)
+
+    #Ausgabe des pred Martensit & Differenz zum Soll & Varianz der 5 Modellierungen
+    # Laden des gespeicherten Modells AdaBoost/Martensit
+    filename = ["2_Model_AdaBoost_vc_ap_f_T_Nr1", "2_Model_AdaBoost_vc_ap_f_T_Nr2", "2_Model_AdaBoost_vc_ap_f_T_Nr3"
+        , "2_Model_AdaBoost_vc_ap_f_T_Nr4", "2_Model_AdaBoost_vc_ap_f_T_Nr5"]
+    martensit_list = []
+
+    for i in filename:
+        file = os.path.join(path, i)
+        loaded_model = pickle.load(open(file, 'rb'))
+
+        martensit_list.append(loaded_model.predict(solution_list.reshape(1,-1)))
+
+    print('\nMartensit pred: ', martensit_list[3])
+    print('Martensit soll: ',m_target)
+
+    m_diff = abs(m_target-martensit_list[3])
+    print("Martensit Diff: ",m_diff)
+
+    print('Martensit pred der 5 Modellierungen: ', martensit_list)
+
+    varianz = np.var(martensit_list)
+    print("Varianz: ", varianz)
